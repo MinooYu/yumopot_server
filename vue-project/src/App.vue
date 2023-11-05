a470f165a49d4946a999edc2331a22be2e9ba118
MinooYu <yidu24zwolf@gmail.com>
2023-11-06 01:20:35 +0900

    探索

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="mlr-a w-152" v-for="(row, i) in rows" :key="i" style="display: flex;">
+	<div class="mlr-a w-152" v-for="(row, i) in minesrow" :key="i" style="display: flex;">
 		<div class="mlr-a" v-for="(data, j) in row" :key="j">
 			<div class="minescell" style="text-align: center; background-color: #eee; border-radius: 4px; margin: 4px;">
 				<a>{{ data }}</a>
@@ -14,7 +14,7 @@ export default {
 	data: () => (
 		{
 			mines: [],
-			rows: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+			minesrow: [['','','',''],['','','',''],['','','',''],['','','','']],
 			minescnt: 4,
 		}
 	),
@@ -25,9 +25,35 @@ export default {
 			var x = getRandomInt(4)
 			var y = getRandomInt(4)
 
-			if(this.rows[x][y] == 1) { i--;	continue; }
+			if(this.minesrow[x][y] == "t") { i--;	continue; }
 
-			this.rows[x][y] = 1
+			this.minesrow[x][y] = "t";
+		}
+
+		for(var a = 0; a < this.minescnt; a++)
+		{
+			for(var b = 0; b < this.minescnt; b++)
+			{
+				if (this.minesrow[a][b] == "t") continue;
+
+				var cnt = 0;
+				if (a-1>=0) {
+					if (b-1>=0 && this.minesrow[a-1][b-1] == "t") cnt++;
+					if (this.minesrow[a-1][b] == "t") cnt++;
+					if (b+1<4 && this.minesrow[a-1][b+1] == "t") cnt++;
+				}
+
+				if(b-1>=0 && this.minesrow[a][b-1] == "t") cnt++;
+				if(b+1<4 && this.minesrow[a][b+1] == "t") cnt++;
+
+				if(a+1<4) {
+					if(b-1>=0 && this.minesrow[a+1][b-1] == "t") cnt++;
+					if(this.minesrow[a+1][b] == "t") cnt++;
+					if(b+1<4 && this.minesrow[a+1][b+1] == "t") cnt++;
+				}
+
+				this.minesrow[a][b] = String(cnt);
+			}
 		}
 
 	},
