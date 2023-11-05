54779d2dc753bf5b2fe2122570fe8e7eb66191c5
MinooYu <yidu24zwolf@gmail.com>
2023-11-05 23:30:32 +0900

    init

---
+++ vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+	data: () => (
+		{
+			mines: [],
+			rows: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+		}
+	),
+	mounted() {
+
+	},
+	methods: {
+
+	}
+}
+
+
+</script>
+
+<template>
+	<div class="mlr-a" v-for="(row, i) in rows" :key="i" style="width: 254px; display: flex;">
+		<div v-for="(data, j) in row" :key="j" style="width: 122px;">
+			<div style="width: 16px; text-align: center;" class="mlr-a">
+				<a>{{ data }}</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.mlr-a {
+	margin-left: auto; margin-right: auto;
+}
+</style>
